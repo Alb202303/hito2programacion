b51dc7de6a4a0fbbbfabb1a7964f8e39fabf99be
AlberFP <93380153+AlberFP@users.noreply.github.com>
2022-03-31 23:52:45 +0200

    Delete hitoprogramacion_albertodiaz directory

--- hitoprogramacion_albertodiaz/config.php
+++
@@ -1,8 +0,0 @@
-<?php
- 
-try {
-    $connection = new PDO('mysql:host=localhost;dbname=basehito2', 'root');
-} catch (PDOException $e) {
-    exit("Error: " . $e->getMessage());
-}
-?>
--- hitoprogramacion_albertodiaz/eliminar.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-if(isset($_POST['eliminar'])){
-    $username = $_POST['usuario'];
-    $resultado=$conexion1->query("DELETE FROM usuario WHERE usuario='{$_SESSION['usuario']}'");
-
-}
